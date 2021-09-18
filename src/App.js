83c51cd8f61c0ea331c4392930157d634b469f7a
Hasan75 <hasanjab14@gmail.com>
2021-09-18 17:13:18 +0600

    api called using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react/cjs/react.development';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Counter></Counter>
+    <LoadComments></LoadComments>
     </div>
   );
+}
+function LoadComments (){
+  const [comments, setComments] = useState([])
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setComments(data))
+  }, [])
+  return (
+    <div>
+      <h3>Load Comments</h3>
+      {
+      comments.map(comment => <Comment title={comment.title} body={comment.body}></Comment>)
+      }
+    </div>
+  )
+}
+
+function Comment (props) {
+  return (
+    <div style={{backgroundColor:'skyblue', margin: '20px', padding: '15px', border: '3px solid grey', borderRadius: '15px'}}>
+      <h3>Title: {props.title}</h3>
+      <p>Comment: {props.body} </p>
+    </div>
+  )
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseHandle = () => setCount(count + 1)
+  const decreaseHandle = () => setCount(count - 1)
+  return(
+    <div>
+      <h3>Count: {count} </h3>
+      <button onClick={increaseHandle}>Increase</button>
+      <button onClick={decreaseHandle}>Decrease</button>
+    </div>
+  )
 }
 
 export default App;
